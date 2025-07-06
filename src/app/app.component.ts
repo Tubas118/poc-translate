fff99ad5bef90855f2167c37ec8aeca9ecdfe56e
Tony Duncan <Tubas118@users.noreply.github.com>
2025-07-06 14:18:51 -0500

    Initial impl for ngx-translate only

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,14 +10,41 @@ import { TranslateService } from '@ngx-translate/core';
 export class AppComponent {
   title = 'poc-translate';
 
+  private readonly supportedLanguagesMap = new Map<string, string>([
+    ['en', 'supported.english'],
+    ['es', 'supported.spanish']
+  ]);
+
+  private readonly translateToLanguageCodeMap: Map<string, string>;
+  
+  private readonly supportedLanguageCodes = Array.from(this.supportedLanguagesMap.keys());
+
+  private readonly supportedLanguageTranslateIds = Array.from(this.supportedLanguagesMap.values());
+
   constructor(private translate: TranslateService) {
-    this.translate.addLangs(['en', 'es']);
+    this.translate.addLangs(this.supportedLanguageCodes);
     this.translate.setDefaultLang('en');
     this.translate.use('en');
+
+    this.translateToLanguageCodeMap = new Map<string, string>();
+    this.supportedLanguagesMap.forEach((value, key) => {
+      console.log(`key: ${value}, value: ${key}`);
+      this.translateToLanguageCodeMap.set(value, key);
+    });
   }
 
-  getAvailableLanguages(): string[] {
+  getAvailableLanguageCodes(): string[] {
     return this.translate.getLangs();
+  }
+
+  getAvailableLanguageTranslateIds(): string[] {
+    return this.supportedLanguageTranslateIds;
+  }
+
+  onLanguageSelectionChange(event:Event) {
+    const translateId = (event.target as HTMLSelectElement).value;
+    const code = this.translateToLanguageCodeMap.get(translateId) || this.translate.defaultLang;
+    this.translate.use(code);
   }
 
 }
